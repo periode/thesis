c02aa909dab603c018fa34adb5b1c0306baa6781
période <pierre.depaz@gmail.com>
2023-02-14 18:22:12 +0100

    replaced inline code by file import in introduction

---
+++ redaction/corpus/simple.py
@@ -0,0 +1,7 @@
+    a = 4
+    b = 6
+
+    def compute(first, second):
+        return (first * 2) + second
+    
+    compute(a, b)
---
+++ redaction/corpus/verbose_refactored.c
@@ -0,0 +1,6 @@
+void leftpad(int i)
+{
+    char *c;
+    if (i >= 0 && i < 10)
+        c = '0' + i;
+}
